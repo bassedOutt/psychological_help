d0254999818f328c86369d548ac1107a8a9bb2fa
bassedOutt <77404024+bassedOutt@users.noreply.github.com>
2022-11-20 15:12:24 +0200

    inital commit

---
+++ src/main/java/com/polytech/psychological_help/PsychologicalHelpApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.psychological_help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalHelpApplication.class, args);
+    }
+
+}
