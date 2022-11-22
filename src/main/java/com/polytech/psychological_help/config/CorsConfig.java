76f4f4fbe3b4a6e5302750023d64464ad1cb9dfb
bassedOutt <77404024+bassedOutt@users.noreply.github.com>
2022-11-22 02:03:07 +0200

    fix cors

---
+++ src/main/java/com/polytech/psychological_help/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.polytech.psychological_help.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsConfig extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "*");
+        response.addHeader("Access-Control-Allow-Headers", "*");
+        filterChain.doFilter(request, response);
+    }
+}
